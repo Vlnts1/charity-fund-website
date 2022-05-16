e41a677bd82f4a73641931d6c4d5a72c7fb4edb8
Vlnts <valentsionovaviktoria@gmail.com>
2022-05-16 19:58:34 +0300

    commit

--- Client/charity-fund/src/Components/Header/Header.jsx
+++ Client/charity-fund/src/Components/Header/Header.jsx
@@ -1,9 +1,8 @@
-import React, {Link} from 'react';
-import {Container, Col, Row, Nav, Ratio, Card } from 'react-bootstrap'
-import Menu from './Menu'
+import React from 'react';
+import {Container, Nav, Navbar,NavDropdown } from 'react-bootstrap'
+import {Link} from "react-router-dom";
 import SocialFollow from "../Header/SocialFollow"
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import logomain from "../../assets/logomain.jpg"
 
 function Header(  {isLoggedIn, setIsLoggedIn, userName, setIsAdmin } ) {
 
@@ -13,40 +12,44 @@ function Header(  {isLoggedIn, setIsLoggedIn, userName, setIsAdmin } ) {
     setIsLoggedIn(false);
     setIsAdmin(false);
   };
+
   return (
     <div className="App">
-  <nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <span class="navbar-text">
-    { 
-          isLoggedIn ?
-          <nav style={{textAlign: "center"}}>
-        Адмін, <strong> {userName} </strong>
-        <Nav.Link onClick={handleLogOut} exact to="/login"> <ExitToAppIcon/> Logout</Nav.Link>
-        </nav>
-        : 'dyxovnistnacii@gmail.com'
-        
-      }
-      </span>
-    <span class="navbar-text">
-      <SocialFollow/>
-    </span>
-  </div>
-</nav>
-        <Container>
-        <Row>
-          <Col>
-          <Ratio aspectRatio={50} style={{margin: "2% auto"}}>
-          <Card.Img variant="top" src={logomain} />
-          </Ratio>
-          </Col>
-          <Col></Col>
-          <Col></Col>
-          <br></br>
-        </Row>
-        </Container>
-         
-          <Menu/>
+      <Navbar bg="light" expand="lg" fixed="top">
+          <Container fluid>
+            <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: '100px' }}
+                  navbarScroll
+                >
+                  <Nav.Link as={Link} to={"/"}>Головна</Nav.Link>
+                  <Nav.Link as={Link} to={"/about"}>Про нас</Nav.Link>
+                  <Nav.Link as={Link} to={"/projects"}>Проекти</Nav.Link>
+                  <NavDropdown title="Новини" id="navbarScrollingDropdown">
+                    <NavDropdown.Item as={Link} to={"/blog"}>Блог</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to={"/reports"}>Звіти</NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link as={Link} to={"/contacts"}>Контакти</Nav.Link>
+                </Nav>  
+              <span class="text" style={{textAlign: "center"}}>
+                { 
+                  isLoggedIn ?
+                    <nav style={{textAlign: "center"}}>
+                    Адмін, <strong> {userName} </strong>
+                    <Nav.Link onClick={handleLogOut} exact to="/login"> <ExitToAppIcon/> Logout</Nav.Link>
+                    </nav>
+                  : 'dyxovnistnacii@gmail.com'
+                }
+                </span>
+                <span class="navbar-text">
+                  <SocialFollow/>
+                </span>
+            </Navbar.Collapse>
+          </Container>
+      </Navbar>        
     </div>
     
   );
